866afa36e46a346b4878c8c3e46f768af12357db
Matt Bush <59295261+matthewbush55@users.noreply.github.com>
2021-09-08 08:34:14 -0600

    Added API routes

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -1,27 +1,43 @@
-const router = require('express').Router();
-const { Category, Product } = require('../../models');
+const router = require("express").Router();
+const { Category, Product } = require("../../models");
 
 // The `/api/categories` endpoint
 
-router.get('/', (req, res) => {
+router.get("/", (req, res) => {
   // find all categories
+  Categories.findAll()
+    .then((catData) => {
+      res.json(catData);
+    })
+    .catch((err) => {
+      console.log(err);
+    });
   // be sure to include its associated Products
 });
 
-router.get('/:id', (req, res) => {
+router.get("/:id", (req, res) => {
   // find one category by its `id` value
   // be sure to include its associated Products
 });
 
-router.post('/', (req, res) => {
+router.post("/", (req, res) => {
   // create a new category
+  Categories.Create({
+    category_name: req.body.category_name,
+  })
+    .then((catData) => {
+      res.json(catData);
+    })
+    .catch((err) => {
+      console.log(err);
+    });
 });
 
-router.put('/:id', (req, res) => {
+router.put("/:id", (req, res) => {
   // update a category by its `id` value
 });
 
-router.delete('/:id', (req, res) => {
+router.delete("/:id", (req, res) => {
   // delete a category by its `id` value
 });
 
